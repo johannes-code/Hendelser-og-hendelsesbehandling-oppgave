304456b5d008eb0c5d9be87aa4cde1f06e0a23d7
johannes-code <johannes24sfj@gmail.com>
2024-12-10 11:28:40 +0100

    added more sounds, now one sound at the time plays, and keyboard keys works

--- data/soundsJS.js
+++ data/soundsJS.js
@@ -8,6 +8,12 @@ export const sounds = [
     { "file": "sounds/kenny-dies.mp3 " , "key":"y"},
     { "file": "sounds/meow-1.mp3 " , "key":"u"},
     { "file": "sounds/pokemon-theme-song-original2.mp3 " , "key":"i"},
-    { "file": "sounds/tom-and-jerry-the-best-scream.mp3 " , "key":"o"}
-  
+    { "file": "sounds/tom-and-jerry-the-best-scream.mp3 " , "key":"o"},
+    { "file": "sounds/blackjack-and-hookers-the-great-and-powerful-trixie-futurama-mo1p-gojsqe.mp3 " , "key":"p"},
+    { "file": "sounds/tmnt-1987.mp3 " , "key":"a"},
+    { "file": "sounds/diddy-done.mp3 " , "key":"s"},
+    { "file": "sounds/jigsaw-doll.mp3 " , "key":"d"},
+    { "file": "sounds/troll-laugh.mp3 " , "key":"f"},
+    { "file": "sounds/oh-no-no-no-no-laugh_sejx5Bk.mp3 " , "key":"g"}
+    
 ]
--- js/soundboard_oppgave.js
+++ js/soundboard_oppgave.js
@@ -3,32 +3,56 @@ import {sounds} from '/data/soundsJS.js';
 console.log(sounds);
 
 
+
 document.getElementById('drumkit');
 
 console.log(drumkit);
 
+let currentAudio = null;
+
+
+
 sounds.map((sound) => {
     /*lager knapp*/
+    // const audio = document.createElement('audio');
+    const audio = new Audio(sound.file)
     const btn = document.createElement('Button');
     drumkit.appendChild(btn);
     console.log(btn);
     /*setter sound.key til text content på knapp, henter fra soundsJS */
     btn.textContent = sound.key;
-    const audio = document.createElement('audio');
     audio.src = sound.file;
+    audio.id = sound.file;
     console.log(sound.file);
     btn.addEventListener('click', () => {
-        audio.play();
+        // audio.play();
+        playSound(audio);
     });
-    document.addEventListener('keypress', (e) => {
-        if (e.key === sound.key) {
-            audio.play();
-            console.log(`Key ${sound.key} was pressed`)
-            
-        }
-    })
     
-})
+    
+    document.addEventListener('keypress', (e) => {
+        // const sound = sounds.find(s => s.key === e.key);
+        if (e.key === sound.key) {
+            playSound(audio);
+        }
+    });
+    
+    function playSound(audio) {
+        if (currentAudio && currentAudio !== audio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+
+        if (currentAudio !== audio) {
+            audio.currentTime = 0;
+            audio.play();
+            currentAudio = audio;
+        }
+        audio.play();
+    }});
+
+
+
 
 
 //2.4. OPTIONAL. If you used keydown as the first parameter in the previous eventlistener, add another eventlistner to the whole page that: 
@@ -41,19 +65,3 @@ sounds.map((sound) => {
 
 //2.5. OPTIONAL. Create an eventlistener for clicking. Also create a logic for preventing more sounds to be played at the same time
 
-//2.6. append the created button and audio element to the html element you refered in 1.
-
-
-//* ///////////////////////////////////////////////////////////////////////////////////////////// */
-
-
-//*3. Create a function that loops over the sounds (from the data file you created). Use that function created in 2. to use the logic there to create the buttons. I prefer that you use .forEach or .map */
-
-
-//* ////////////////////////////////////// */
-
-
-//*4. Call on the function that loops over the sounds and creates the buttons */
-
-
-//* ////////////////////////////////////// */
